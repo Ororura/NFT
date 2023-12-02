37bccb18e32e2ba4f3fc9749fea5e42a1b9de577
Ororura <hitmanlol890@gmail.com>
2023-12-02 17:21:11 +0300

    add changePrice and transferNFC

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -16,6 +16,7 @@ contract NFT is ERC1155("") {
 
     struct Asset {
         uint id;
+        uint _idx;
         string name;
         string desc;
         string img;
@@ -90,8 +91,8 @@ contract NFT is ERC1155("") {
     function createAsset(uint _id, string memory _name, string memory _desc, string memory _img, uint _price, uint _releasedAmount, uint _dateCreate) public OnlyOwner {
         require(assets[_id].price == 0, unicode"NFT с таким id уже есть в системе");
         _mint(msg.sender, _id, _releasedAmount, "");
-        assets[_id] = Asset(_id, _name, _desc, _img, _price, _releasedAmount, _dateCreate);
-        userAssets[msg.sender].push(Asset(_id, _name, _desc, _img, _price, _releasedAmount, _dateCreate));
+        assets[_id] = Asset(_id, userAssets[msg.sender].length, _name, _desc, _img, _price, _releasedAmount, _dateCreate);
+        userAssets[msg.sender].push(Asset(_id, userAssets[msg.sender].length, _name, _desc, _img, _price, _releasedAmount, _dateCreate));
     }
 
     function createCollection(uint _id, string memory _name, string memory _desc, uint[] memory _ids, uint[] memory _amounts) public OnlyOwner {
@@ -122,14 +123,14 @@ contract NFT is ERC1155("") {
         sells.push(AssetSell(sells.length, _id, _AssetIdx, msg.sender, _amount, _price));
     }
 
-    function buyNFT(uint _id, uint _amount, uint _discount) external {
+    function buyAsset(uint _id, uint _amount, uint _discount) external {
         uint totalPrice = sells[_id].price * _amount - (sells[_id].price * _amount * _discount / 100);
         require(sells[_id].amount >= _amount, unicode"Вы пытаетесь купить больше, чем есть");
         _safeTransferFrom(sells[_id].seller, msg.sender, sells[_id].assetId, _amount, "");
         token.transferToken(msg.sender, sells[_id].seller, totalPrice);
         sells[_id].amount -= _amount;
         userAssets[sells[_id].seller][sells[_id].assetId].releasedAmount -= _amount;
-        userAssets[msg.sender].push(Asset(userAssets[msg.sender].length, assets[sells[_id].assetId].name, assets[sells[_id].assetId].desc, assets[sells[_id].assetId].img, _amount, sells[_id].price, assets[sells[_id].assetId].dateCreate));
+        userAssets[msg.sender].push(Asset(_id, userAssets[msg.sender].length, assets[sells[_id].assetId].name, assets[sells[_id].assetId].desc, assets[sells[_id].assetId].img, _amount, sells[_id].price, assets[sells[_id].assetId].dateCreate));
 
         if(userAssets[sells[_id].seller][sells[_id].assetId].releasedAmount == 0) {
             delete userAssets[sells[_id].seller];
@@ -138,7 +139,15 @@ contract NFT is ERC1155("") {
         if(sells[_id].amount == 0) {
             delete sells[_id];
         }
+    }
 
+    function transferAsset(uint _id, uint _idx, address _receiver, uint _amount) external {
+        _safeTransferFrom(msg.sender, _receiver, _id, _amount, "");
+        userAssets[msg.sender][_idx].releasedAmount -= _amount;
+        userAssets[_receiver].push(Asset(_id, userAssets[_receiver].length, assets[_id].name, assets[_id].desc, assets[_id].img, _amount, userAssets[msg.sender][_idx].price, assets[_id].dateCreate));
+        if(userAssets[msg.sender][_idx].releasedAmount == 0) {
+            delete userAssets[msg.sender][_idx];
+        }
     }
 
     function startAuc(uint _collectionId, uint _timeStart, uint _timeEnd, uint maxPrice, uint minPrice, address leader, uint currentBet) public OnlyOwner{
@@ -146,6 +155,11 @@ contract NFT is ERC1155("") {
             require(auctionArray[i].collectionId != _collectionId, unicode"Аукцион с такой коллекцией уже запущен");
         }
         auctionArray.push(Auction(auctionArray.length, _collectionId, _timeStart, _timeEnd, maxPrice, minPrice, leader, currentBet));
+    }
+
+    function changeSellPrice(uint _idx, uint _price) public {
+        require(sells[_idx].seller == msg.sender, unicode"Это не ваш лот");
+        sells[_idx].price = _price * dec;
     }
 
     function getReferrals() public view returns(ReferralCode[] memory) {
